de71a9f093825714c1db478031589da7c519c951
Aleksandar <alexplamenov1@gmail.com>
2018-05-08 02:41:50 -0700

    Nueva clase

---
+++ HellowWorld2/src/HelloWorld21.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld21 {
+
+	public HelloWorld21() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola2");
+	}
+
+}
