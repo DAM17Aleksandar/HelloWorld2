eebb8f5bebe0af134810b8d2f59ff8c18cd4c221
Aleksandar <alexplamenov1@gmail.com>
2018-05-08 02:07:57 -0700

    Nueva version

---
+++ HellowWorld2/src/HelloWorld2.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld2 {
+
+	public HelloWorld2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola3");
+	}
+
+}
