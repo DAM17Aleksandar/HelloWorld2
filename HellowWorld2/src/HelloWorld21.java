440cd2a4f6ffc6c9c919470750db135e96cc2b79
Aleksandar <alexplamenov1@gmail.com>
2018-05-08 02:46:43 -0700

    Nueva versión

--- HellowWorld2/src/HelloWorld21.java
+++ HellowWorld2/src/HelloWorld21.java
@@ -7,7 +7,7 @@ public class HelloWorld21 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola2");
+		System.out.println("Hola2 Modificado para la versión 2");
 	}
 
 }
